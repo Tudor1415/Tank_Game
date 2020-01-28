71668e3336e37213cb049bd47226300b6202c4da
Tudor1415 <44468967+Tudor1415@users.noreply.github.com>
2020-01-28 08:57:58 +0100

    added install script

---
+++ install.py
@@ -0,0 +1,8 @@
+import subprocess
+import sys
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+install('numpy')
+install('arcade')
+install('sklearn')

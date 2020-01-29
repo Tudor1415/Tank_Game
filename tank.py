3dedd1a97f933b98d5e5505a714123875e9d9663
timdu92 <35028955+timdu92@users.noreply.github.com>
2020-01-29 18:36:44 +0100

    Create tank.py

---
+++ tank.py
@@ -0,0 +1,13 @@
+import arcade
+import os
+
+arcade.open_window(1200, 600, "Tank Test")
+
+arcade.start_render() #doit être fait avant chaque commande de dessin
+
+
+
+
+
+arcade.finish_render() #doit être fait à la fin du dessin
+arcade.run() #garde la fenêtre ouverte jusqu'à ce que quelqu'un la ferme

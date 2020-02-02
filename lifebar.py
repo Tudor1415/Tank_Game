7aec123cb302dc95d2242163055472514e8645dd
timdu92 <35028955+timdu92@users.noreply.github.com>
2020-02-02 18:53:59 +0100

    Update lifebar.py

--- lifebar.py
+++ lifebar.py
@@ -2,10 +2,16 @@ import arcade
 import os
 import tank
 
-#def vie():
+def vie():
+    hpbleu,hprouge=100
+    if hpbleu==0:
+        arcade.draw_text("Tank Bleu est mort ! Victoire du Tank Rouge !")
+    if hprouge==0:
+        arcade.draw_text("Tank Rouge est mort ! Victoire du Tank Bleu !")
+
+def barre():
+    arcade.draw_rectangle_outline(width=100,height=10,arcade.color.WHITE,border_width=2)
+    if hp==100:
 
 
-hp=100
-
-arcade.draw_rectangle_outline()
 

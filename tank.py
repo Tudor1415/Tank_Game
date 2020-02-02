4cd52ca042682eb9adc9a2b0e1b306b54a6c407c
timdu92 <35028955+timdu92@users.noreply.github.com>
2020-02-02 17:56:17 +0100

    Update tank.py

--- tank.py
+++ tank.py
@@ -1,11 +1,16 @@
+#http://arcade.academy/examples/drawing_primitives.html
 import arcade
 import os
 
-arcade.open_window(1200, 600, "Tank Test")
+arcade.open_window(1200, 600, "Tanks",antialiasing=True, resizable=True)
 
 arcade.start_render() #doit être fait avant chaque commande de dessin
 
-arcade.draw_rectangle_outline(295, 100, 45, 65,arcade.color.BRITISH_RACING_GREEN)
+arcade.draw_rectangle_filled(600, 300, 100, 150,arcade.color.BLUE) #rectangle du tank bleu
+arcade.draw_rectangle_filled(600,370,30,150,arcade.color.BLUE) #rectangle du canon du tank bleu
+arcade.draw_rectangle_outline(600,300,100,150,arcade.color.WHITE,border_width=2) #contour du tank bleu en blanc
+arcade.draw_rectangle_outline(600,370,30,150,arcade.color.WHITE)
+arcade.draw_circle_filled(600,300,20,arcade.color.DARK_BLUE)
 
 
 

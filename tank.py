08accca3aff613b4d4ce3c4fdaab7ba2a85de94c
timdu92 <35028955+timdu92@users.noreply.github.com>
2020-02-03 17:51:06 +0100

    Update tank.py

--- tank.py
+++ tank.py
@@ -18,14 +18,14 @@ def tank_rouge():
     arcade.draw_rectangle_outline(300,370,30,150,arcade.color.WHITE)#contour du canon du tank rouge
     arcade.draw_circle_filled(300,300,20,arcade.color.DARK_BLUE) #pivot du canon
 
- def vie():
+ def vie(hpbleu,hprouge):
     hpbleu,hprouge=100
     if hpbleu==0:
         arcade.draw_text("Tank Bleu est mort ! Victoire du Tank Rouge !")
     if hprouge==0:
         arcade.draw_text("Tank Rouge est mort ! Victoire du Tank Bleu !")
 
-def barrebleu(vie):
+def barrebleu(x,y):
     arcade.draw_rectangle_outline(width=100,height=10,arcade.color.WHITE,border_width=2)
     if hpbleu==100:
         arcade.draw_rectangle_filled(width=100,height=10,arcade.color.LIGHT_BLUE)
@@ -33,9 +33,29 @@ def barrebleu(vie):
         arcade.draw_rectangle_filled(width=80,height=10,arcade.color.LIGHT_BLUE)
     elif hpbleu==60:
         arcade.draw_rectangle_filled(width=60,height=10,arcade.color.LIGHT_BLUE)
+    elif hpbleu==40:
+        arcade.draw_rectangle_filled(width=40,height=10,arcade.color.LIGHT_BLUE)
+    elif hpbleu==20:
+        arcade.draw_rectangle_filled(width=20,height=10,arcade.color.LIGHT_BLUE)
+    elif hpbleu==0:
+        vie(hpbleu)
 
-arcade.finish_render() #doit être fait à la fin du dessin
-arcade.run() #garde la fenêtre ouverte jusqu'à ce que quelqu'un la ferme
+def barrerouge(x,y):
+    arcade.draw_rectangle_outline(x,y-20,width=100,height=10,arcade.color.WHITE,border_width=2)
+    if hprouge==100:
+        arcade.draw_rectangle_filled(x,width=100,height=10,arcade.color.LIGHT_BLUE)
+    elif hprouge==80:
+        arcade.draw_rectangle_filled(width=80,height=10,arcade.color.LIGHT_BLUE)
+    elif hprouge==60:
+        arcade.draw_rectangle_filled(width=60,height=10,arcade.color.LIGHT_BLUE)
+    elif hprouge==40:
+        arcade.draw_rectangle_filled(width=40,height=10,arcade.color.LIGHT_BLUE)
+    elif hprouge==20:
+        arcade.draw_rectangle_filled(width=20,height=10,arcade.color.LIGHT_BLUE)
+    elif hprouge==0:
+        vie(hprouge)
+
+
 
 def main():
     arcade.open_window(1200, 600, "Tanks",antialiasing=True, resizable=True)
@@ -43,6 +63,9 @@ def main():
 
     tank_bleu()
     tank_rouge()
+
     
+    arcade.finish_render() #doit être fait à la fin du dessin
+    arcade.run() #garde la fenêtre ouverte jusqu'à ce que quelqu'un la ferme
 
 
